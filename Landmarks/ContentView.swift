28f68d71a908e3d8de2c98ba43242d80dd5a4095
kojoma <masaru.kojo@gmail.com>
2019-10-19 17:11:43 +0900

    Create Landmarks project

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by masaru kojo on 2019/10/19.
+//  Copyright © 2019 masaru kojo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
