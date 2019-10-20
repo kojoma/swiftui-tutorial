6bc31efdc9f61fe343651df1407b4f5686a40ad5
kojoma <masaru.kojo@gmail.com>
2019-10-20 13:49:16 +0900

    Section 4: Create the List of Landmarks

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by masaru kojo on 2019/10/20.
+//  Copyright © 2019 masaru kojo. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
